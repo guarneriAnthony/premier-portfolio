476dd58f067472f9ea943e29a35a9733fb7067d8
guarneri anthony <guarnerianthony@outlook.fr>
2022-02-07 17:52:31 +0100

    commit global de siga france

---
+++ menu/menu.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+ 	<link rel="stylesheet" type="text/css" href="main.css">
+ 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+ 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+ 	<script src="main.js"></script>
+	<title>menu php </title>
+</head>
+<body>
+		<div class="container">
+			<nav class="navbar navbar-fixed-top">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<div id="myNavbar" class="collapse navbar-collapse">
+					<ul id="menu">
+						<div class="pages col-lg-2 col-md-4 col-xs-12"><li><a href="#"> A Propos </a></li></div>
+						<div class="pages col-lg-2 col-md-4 col-xs-12"><li><a href="#"> Competences </a></li></div>
+						<div class="pages col-lg-2 col-md-4 col-xs-12"><li><a href="#"> Parcours </a></li></div>
+						<div class="pages col-lg-2 col-md-4 col-xs-12"><li><a href="#"> Formations </a></li></div>
+						<div class="pages col-lg-2 col-md-4 col-xs-12"><li><a href="#"> Portfolio </a></li></div>
+						<div class="pages col-lg-2 col-md-4 col-xs-12"><li><a href="#"> Commentaires </a></li></div>
+					</ul>
+				</div>
+			</nav>	
+			<div class="row navbar-fixed-bottom">
+				<div class="imgReseaux pages col-lg-5 col-md-5 col-xs-5"><a href="https://www.facebook.com/anthony.guarneri.792"><img src="img/facebook.png"></a></div>
+				<div class="imgReseaux pages col-lg-2 col-md-2 col-xs-2"><a href="https://www.linkedin.com/in/anthony-guarneri-781418153/"><img src="img/linkedin.png"></a></div>
+				<div class="imgReseaux pages col-lg-5 col-md-5 col-xs-5"><a href="https://www.instagram.com/guarneri.anthony/"><img src="img/instagram42.png"></a></div>
+			</div>		
+		</div>
+</body>
+</html>
